539dcfe12e5e5053ff2211735307b9ea525b18be
mustafemohamoud <mustyymo@outlook.com>
2024-03-14 20:55:44 +0000

    index.js and category.js done

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -16,5 +25,4 @@ Category.init(
     modelName: 'category',
   }
 );
-
 module.exports = Category;
